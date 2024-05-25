2b2789a2ec3bbde6ab6347664bc089de814b3a96
AbuZobayed <zobayed.ict05@gmail.com>
2024-05-26 00:44:40 +0600

    create a functon(handleDeleProduct) useing filter methed for delete product

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -9,13 +9,18 @@ export default function AllProducts() {
         .then((res)=>res.json())
         .then((data) => setProducts(data)) 
     },[])
+
+    const handleDeleteProduct = (id) =>{
+      setProducts(products.filter((product) => product.id !== id));
+    };
+
   return (
     <div>
       <h1 className="text-5xl font-bold text-center">All Products</h1>
       <div className="my-16 flex flex-wrap  gap-4">
         {
             products.map((shoe) => 
-            <SingleProductCardDashboard key={shoe.id} shoe={shoe} />
+            <SingleProductCardDashboard key={shoe.id} shoe={shoe} onDelete={handleDeleteProduct} />
             
             )
         }
