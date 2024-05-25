1f657cb72f876f484e737efafd5d614ae42e86bd
AbuZobayed <zobayed.ict05@gmail.com>
2024-05-25 22:59:02 +0600

    create a add product and get the data

--- src/pages/AddProducts.jsx
+++ src/pages/AddProducts.jsx
@@ -1,10 +1,27 @@
 export default function AddProducts() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target; 
+    const id = form.id.value ;
+    const title = form.title.value;
+    const brand = form.brand.value;
+    const price = form.price.value;
+    const dscription = form.dscription.value;
+    const image_url = form.image_url.value;
+
+    const data = {image_url,title,brand,price,dscription,id};
+
+    console.log(data); 
+
+  }
   return (
     <div>
       <h1 className="text-5xl font-bold text-center">Add a Product</h1>
 
       <div className="my-16">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mt-3">
             <input
               className="bg-gray-100 p-4 w-full border rounded-lg border-indigo-600 "
@@ -49,7 +66,7 @@ export default function AddProducts() {
             <input
               className="bg-gray-100 p-4 w-full border rounded-lg border-indigo-600 "
               type="text"
-              name="image_url"
+              name="id"
               placeholder="ID"
             />
           </div>
