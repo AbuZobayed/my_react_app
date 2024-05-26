1b04fec9b58ac0ac1281d883927e4c492304bc93
AbuZobayed <zobayed.ict05@gmail.com>
2024-05-27 00:18:53 +0600

    dashboard page update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,311 @@
 const Dashboard = () => {
-  return <div>
-    <h2>This is DeshBoard</h2>
-  </div>;
+  return (
+    <div className="container  mx-auto">
+      <h1 className="ms-64 mb-8 text-5xl font-bold text-violet-600">
+        {" "}
+        Dashboard
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Favorite shoe</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="text-sm opacity-50">United States</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Zemlak, Daniel and Leannon
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  Desktop Support Technician
+                </span>
+              </td>
+              <td>Boat shoes</td>
+              <th>
+                <button className="btn btn-ghost bg-violet-500 text-white btn-xs">details</button>
+              </th>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="/src/assets/img/user-1.jpg"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Brice Swyre</div>
+                    <div className="text-sm opacity-50">China</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Carroll Group
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  Tax Accountant
+                </span>
+              </td>
+              <td>Loafer</td>
+              <th>
+                <button className="btn btn-ghost bg-violet-500 text-white btn-xs">details</button>
+              </th>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="/src/assets/img/user-2.jpg"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Marjy Ferencz</div>
+                    <div className="text-sm opacity-50">Russia</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Rowe-Schoen
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  Office Assistant I
+                </span>
+              </td>
+              <td>Sandals</td>
+              <th>
+                <button className="btn btn-ghost bg-violet-500 text-white btn-xs">details</button>
+              </th>
+            </tr>
+            {/* row 4 */}
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="/src/assets/img/user-3.jpg"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Yancy Tear</div>
+                    <div className="text-sm opacity-50">Brazil</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Wyman-Ledner
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  Community Outreach Specialist
+                </span>
+              </td>
+              <td>
+Loafer</td>
+              <th>
+                <button className="btn btn-ghost bg-violet-500 text-white btn-xs">details</button>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="/src/assets/img/user-4.jpg"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Yancy Tear</div>
+                    <div className="text-sm opacity-50">Brazil</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Wyman-Ledner
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  Community Outreach Specialist
+                </span>
+              </td>
+              <td>
+Loafer</td>
+              <th>
+                <button className="btn btn-ghost bg-violet-500 text-white btn-xs">details</button>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="/src/assets/img/user-5.jpg"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Yancy Tear</div>
+                    <div className="text-sm opacity-50">Brazil</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Wyman-Ledner
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  Community Outreach Specialist
+                </span>
+              </td>
+              <td>
+Loafer</td>
+              <th>
+                <button className="btn btn-ghost bg-violet-500 text-white btn-xs">details</button>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="/src/assets/img/user6.jpg"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Yancy Tear</div>
+                    <div className="text-sm opacity-50">Brazil</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Wyman-Ledner
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  Community Outreach Specialist
+                </span>
+              </td>
+              <td>
+Loafer</td>
+              <th>
+                <button className="btn btn-ghost bg-violet-500 text-white btn-xs">details</button>
+              </th>
+            </tr>
+          </tbody>
+          {/* foot */}
+          
+        </table>
+      </div>
+
+      <div className="ms-72 mt-10">
+      <div className="stats w-auto  shadow">
+        <div className="stat ">
+          <div className="stat-title">Total Sales</div>
+          <div className="stat-value text-primary">$25,664</div>
+          <div className="stat-desc text-pink-500">
+            21% more than last month
+          </div>
+        </div>
+
+        <div className="stat ">
+          <div className="stat-title">Site Visit</div>
+          <div className="stat-value text-secondary">20.6k</div>
+          <div className="stat-desc ">21% more than last month</div>
+        </div>
+
+        <div className="stat ">
+          <div className="stat-figure text-secondary">
+            <div className="avatar online">
+              <div className="w-16 rounded-full">
+                <img src="/src/assets/img/user.jpg" />
+              </div>
+            </div>
+          </div>
+          <div className="stat-value">25M</div>
+          <div className="stat-title">Searchs</div>
+          <div className="stat-desc text-secondary">30 days remaining</div>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
