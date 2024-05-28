f9acb841b40ad66f2d92dc4f02ca1d6e0bf84338
AbuZobayed <zobayed.ict05@gmail.com>
2024-05-28 15:08:46 +0600

    Frequently Asked Questions update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,6 @@
 const About = () => {
   return (
-    //     <div className="hero min-h-screen bg-base-200">
-    //   <div className="hero-content flex-col lg:flex-row-reverse">
-    //     <img
-    //       src="/src/assets/img/White Nature Photo Simple Album Cover.png"
-    //     />
-    //     <div>
-    //       <h1 className="text-5xl font-bold">Box Office News!</h1>
-    //       <p className="py-6">
-    //         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-    //         excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-    //         a id nisi.
-    //       </p>
-    //       <button className="btn btn-primary">Get Started</button>
-    //     </div>
-    //   </div>
-    // </div>
+    
     <div className="container mx-auto  m-12">
       <div className="grid grid-cols-2">
         <div className="">
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -62,113 +62,12 @@ const Dashboard = () => {
                 
               </th>
             </tr>
-            {/* row 2 */}
-            {/* <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/src/assets/img/user-1.jpg"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Brice Swyre</div>
-                    <div className="text-sm opacity-50">China</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Carroll Group
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Tax Accountant
-                </span>
-              </td>
-              <td>Loafer</td>
-              <th>
-                <button className="btn btn-ghost bg-violet-500 text-white btn-xs">details</button>
-              </th>
-            </tr> */}
-            {/* row 3 */}
-            {/* <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/src/assets/img/user-2.jpg"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Marjy Ferencz</div>
-                    <div className="text-sm opacity-50">Russia</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Rowe-Schoen
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Office Assistant I
-                </span>
-              </td>
-              <td>Sandals</td>
-              <th>
-                <button className="btn btn-ghost bg-violet-500 text-white btn-xs">details</button>
-              </th>
-            </tr> */}
+            
           </tbody>
-          {/* foot */}
+        
         </table>
       </div>
 
-      {/* anther section  */}
-
-      {/* <div className="ms-72 mt-10">
-      <div className="stats w-auto  shadow">
-        <div className="stat ">
-          <div className="stat-title">Total Sales</div>
-          <div className="stat-value text-primary">$25,664</div>
-          <div className="stat-desc text-pink-500">
-            21% more than last month
-          </div>
-        </div>
-
-        <div className="stat ">
-          <div className="stat-title">Site Visit</div>
-          <div className="stat-value text-secondary">20.6k</div>
-          <div className="stat-desc ">21% more than last month</div>
-        </div>
-
-        <div className="stat ">
-          <div className="stat-figure text-secondary">
-            <div className="avatar online">
-              <div className="w-16 rounded-full">
-                <img src="/src/assets/img/user.jpg" />
-              </div>
-            </div>
-          </div>
-          <div className="stat-value">25M</div>
-          <div className="stat-title">Searchs</div>
-          <div className="stat-desc text-secondary">30 days remaining</div>
-        </div>
-      </div>
-      </div> */}
     </div>
   );
 };
